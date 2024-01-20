c6ac3c1ba2610dbddd3b8570673635f577075563
sia162 <nowitissiya@gmail.com>
2024-01-20 18:11:37 +0530

    created basic express server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("express project");
+const express = require("express");
+const dotenv = require("dotenv").config()
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+//server listening
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+}) 
