6add6ab8b6c7b42dc8db678d36c14aad55a6802c
sia162 <nowitissiya@gmail.com>
2024-01-23 17:15:48 +0530

    created all routes in routes folder

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+//routes middleware
+app.use('/api/contacts',require("./routes/contactRoutes"))
 
 //server listening
 app.listen(port, ()=>{
